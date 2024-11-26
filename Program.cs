7440e414b09532130f534d75ad4ebc3bb76bd09b
nnoizar <alexinjan@hotmail.com>
2024-11-26 12:53:29 +0100

    setup program loop

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//når vi skriver dotnet run så kjører den første linja
-Console.WriteLine("please wright something then hit enter");
-//nå skriver vi noe
-string userInput = Console.ReadLine();
 
-//feilhåndtering
-if (userInput == null){
-    throw new Exception ("Could not read user input");
-}
-switch (userInput){
-    case "borrow":
-    Console.WriteLine("borrrowing a book");
-    break;
-    case "return":
-    Console.WriteLine("return a book");
-    break;
-    default:
-    Console.Write("unrecognized command");
-    break;
+while (true)
+{
+
+    //når vi skriver dotnet run så kjører den første linja
+    Console.WriteLine("please wright something then hit enter");
+    //nå skriver vi noe
+    string userInput = Console.ReadLine();
+
+    //feilhåndtering
+    if (userInput == null)
+    {
+        throw new Exception("Could not read user input");
+    }
+    switch (userInput)
+    {
+        case "borrow":
+            Console.WriteLine("borrrowing a book");
+            break;
+        case "return":
+            Console.WriteLine("return a book");
+            break;
+        default:
+            Console.Write("unrecognized command");
+            break;
+    }
 }
 
 //Console.WriteLine("User input");
